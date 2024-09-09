3e9f8e7b5b62f1fbc1c396d31a9350b312d6bbea
lcs-repo <lcs.fpt.ipo@gmail.com>
2024-09-09 10:31:20 +0800

    test only

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer>
-        <p>&copy; 2024 Sampana Mediclinic and Laboratory. All rights reserved.</p>
+        <p>&copy; 2024 Sampana Mediclinic and Laboratory. All rights reserved...</p>
       </footer>
     )
   }
