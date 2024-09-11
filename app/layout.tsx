d593957ca8332284cfa3082fb14b4f0f3dfaa489
lcs-repo <lcs.fpt.ipo@gmail.com>
2024-09-11 16:43:01 +0800

    favicon ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body>{children}</body>
